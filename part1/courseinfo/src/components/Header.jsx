cde9c1e7f00a7f50981526cb6c059fd96d086c88
Lorenzo Zubani <lorenzozubani1999@gmail.com>
2026-04-02 18:28:11 +0200

    fix: refactoring

--- part1/courseinfo/src/components/Header.jsx
+++
@@ -1,6 +0,0 @@
-const Header = (props) => {
-  const { course } = props 
-  return <h1>{course}</h1>
-}
-
-export default Header
--- part2/phonebook/src/components/FIlter.jsx
+++
@@ -1,9 +0,0 @@
-const Filter = ({ filterName, handleFilterName  }) => {
-  return (
-      <div>
-        filter shown with <input value={filterName} onChange={handleFilterName}/>
-      </div>
-  )
-}
-
-export default Filter
